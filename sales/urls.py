1c72b8a2304a207091a4f9199124af4170184ecf
tim <simwawatimothy64@gmail.com>
2025-09-23 00:33:17 +0200

    completed adding of debit note

--- sales/urls.py
+++ sales/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SaveNormalSale
+from .views import SaveCreditNote, SaveDebitNote, SaveNormalSale
 
 app_name = "sale"
 urlpatterns = [
-    path('api/normal-sale/', SaveNormalSale.as_view())
+    path('api/normal-sale/', SaveNormalSale.as_view()),
+    path('api/create-note/', SaveCreditNote.as_view()),
+    path('api/debit-note/', SaveDebitNote.as_view()),
 ]
--- sales/views.py
+++ sales/views.py
@@ -14,3 +14,28 @@ class SaveNormalSale(APIView):
             return Response({"error": result["error"]}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(result, status=status.HTTP_201_CREATED)
+    
+
+class SaveCreditNote(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        sales_data = request.data
+        ZRA_CLIENT_INSTANCE = SmartInvoiceClient()
+        result = ZRA_CLIENT_INSTANCE.save_sale(sales_data)
+        if 'error' in result:
+            return Response({"error": result["error"]}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(result, status=status.HTTP_201_CREATED)
+    
+class SaveDebitNote(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        sales_data = request.data
+        ZRA_CLIENT_INSTANCE = SmartInvoiceClient()
+        result = ZRA_CLIENT_INSTANCE.save_sale(sales_data)
+        if 'error' in result:
+            return Response({"error": result["error"]}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(result, status=status.HTTP_201_CREATED)
+    
+
